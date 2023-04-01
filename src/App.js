8002443e08951dd2c023b99462b4fc8b4c91932c
Jon Stauffer <staufjon@byu.edu>
2023-03-31 20:11:52 -0600

    mission13

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
+import MovieList from './Body/Movies.tsx';
+import Header from './Header.tsx';
+import Home from './Body/Home.tsx';
+import Podcast from './Body/Podcast.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <body>
+          <Routes>
+            <Route path="/" element={<Home></Home>} />
+            <Route path="/podcast" element={<Podcast></Podcast>} />
+            <Route path="/movies" element={<MovieList></MovieList>} />
+          </Routes>
+        </body>
+      </BrowserRouter>
     </div>
   );
 }
