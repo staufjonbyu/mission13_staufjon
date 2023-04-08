bfa1be1dfb2b93d73924e0569ddec1022ba25476
Jon Stauffer <staufjon@byu.edu>
2023-04-07 21:19:40 -0600

    Revert "connect"
    This reverts commit 87e9f9312dff4d809bd8a6d385398f797b38ba91.

--- src/Body/Movies.tsx
+++ src/Body/Movies.tsx
@@ -1,56 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { Movie } from '../Types/Movie';
+import React, { useState } from 'react';
+import data from '../MovieData.json';
 import './MyStyle.css';
 
-// const MVS = data.MovieData.map((movie) => {
-//   if (!movie.hasOwnProperty('Edited')) {
-//     return { ...movie, Edited: 'No' };
-//   } else {
-//     return movie;
-//   }
-// });
+
+const MVS = data.MovieData.map((movie) => {
+  if (!movie.hasOwnProperty("Edited")) {
+    return { ...movie, Edited: "No" };
+  } else {
+    return movie;
+  }
+});
 
 function MovieList() {
-  const [movieData, setMovieData] = useState<Movie[]>([]);
-
-  useEffect(() => {
-    const fetchMovie = async () => {
-      const rsp = await fetch('https://localhost:4000/movie');
-      const temp = await rsp.json();
-      setMovieData(temp);
-    };
-    fetchMovie();
-  }, []);
+  const [listOfMovies, setListOfMovies] = useState(MVS);
 
   return (
     <>
+
       <div>
         <br></br>
-        <h3 className='custom'>Here's the Movie Collection</h3>
         <table>
+          <caption>Here's the Movie Collection</caption>
           <thead>
             <tr>
-              <th>Category</th>
               <th>Title</th>
               <th>Year</th>
               <th>Director</th>
               <th>Rating</th>
+              <th>Category</th>
               <th>Edited</th>
-              <th>LentTo</th>
-              <th>Notes</th>
             </tr>
           </thead>
           <tbody>
-            {movieData.map((m) => (
-              <tr key={m.movieId}>
-                <td>{m.category}</td>
-                <td>{m.title}</td>
-                <td>{m.year}</td>
-                <td>{m.director}</td>
-                <td>{m.rating}</td>
-                <td>{m.edited}</td>
-                <td>{m.lentTo}</td>
-                <td>{m.notes}</td>
+            {listOfMovies.map((m, index) => (
+              <tr key={index}>
+                <td>{m.Title}</td>
+                <td>{m.Year}</td>
+                <td>{m.Director}</td>
+                <td>{m.Rating}</td>
+                <td>{m.Category}</td>
+                <td>{m.Edited}</td>
               </tr>
             ))}
           </tbody>
@@ -59,5 +48,7 @@ function MovieList() {
     </>
   );
 }
+
+
 
 export default MovieList;
