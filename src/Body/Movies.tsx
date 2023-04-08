87e9f9312dff4d809bd8a6d385398f797b38ba91
Jon Stauffer <staufjon@byu.edu>
2023-04-07 21:06:16 -0600

    connect

--- src/Body/Movies.tsx
+++ src/Body/Movies.tsx
@@ -1,45 +1,56 @@
-import React, { useState } from 'react';
-import data from '../MovieData.json';
+import React, { useState, useEffect } from 'react';
+import { Movie } from '../Types/Movie';
 import './MyStyle.css';
 
-
-const MVS = data.MovieData.map((movie) => {
-  if (!movie.hasOwnProperty("Edited")) {
-    return { ...movie, Edited: "No" };
-  } else {
-    return movie;
-  }
-});
+// const MVS = data.MovieData.map((movie) => {
+//   if (!movie.hasOwnProperty('Edited')) {
+//     return { ...movie, Edited: 'No' };
+//   } else {
+//     return movie;
+//   }
+// });
 
 function MovieList() {
-  const [listOfMovies, setListOfMovies] = useState(MVS);
+  const [movieData, setMovieData] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const rsp = await fetch('https://localhost:4000/movie');
+      const temp = await rsp.json();
+      setMovieData(temp);
+    };
+    fetchMovie();
+  }, []);
 
   return (
     <>
-
       <div>
         <br></br>
+        <h3 className='custom'>Here's the Movie Collection</h3>
         <table>
-          <caption>Here's the Movie Collection</caption>
           <thead>
             <tr>
+              <th>Category</th>
               <th>Title</th>
               <th>Year</th>
               <th>Director</th>
               <th>Rating</th>
-              <th>Category</th>
               <th>Edited</th>
+              <th>LentTo</th>
+              <th>Notes</th>
             </tr>
           </thead>
           <tbody>
-            {listOfMovies.map((m, index) => (
-              <tr key={index}>
-                <td>{m.Title}</td>
-                <td>{m.Year}</td>
-                <td>{m.Director}</td>
-                <td>{m.Rating}</td>
-                <td>{m.Category}</td>
-                <td>{m.Edited}</td>
+            {movieData.map((m) => (
+              <tr key={m.movieId}>
+                <td>{m.category}</td>
+                <td>{m.title}</td>
+                <td>{m.year}</td>
+                <td>{m.director}</td>
+                <td>{m.rating}</td>
+                <td>{m.edited}</td>
+                <td>{m.lentTo}</td>
+                <td>{m.notes}</td>
               </tr>
             ))}
           </tbody>
@@ -48,7 +59,5 @@ function MovieList() {
     </>
   );
 }
-
-
 
 export default MovieList;
